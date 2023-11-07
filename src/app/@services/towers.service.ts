cb0588cac4811235ae481ebf7f650613d5f4f2cf
Maxime Dupont <maxime.dupont49280@gmail.com>
2023-11-07 17:25:14 +0100

    refactoring update tower one

--- src/app/@services/towers.service.ts
+++ src/app/@services/towers.service.ts
@@ -11,55 +11,25 @@ export class TowersService {
   constructor(private gamesService:GamesService) { }
 
   updateTowerOne(game: Game, winner: number | undefined) {
-    let winnerGame: Game = {tower : 2, number: 0}
-    let loserGame: Game = {tower : 2, number: 0}
-    if (game.number === 1) {
-      winnerGame = {
-        ...winnerGame,
-        number: 1,
-        first_player: winner
-      }
-      loserGame = {
-        ...loserGame,
-        number: 3,
-        second_player: winner === game.first_player ? game.second_player : game.first_player
-      }
-    } else if (game.number === 2) {
-      winnerGame = {
-        ...winnerGame,
-        number: 2,
-        first_player: winner
-      }
-      loserGame = {
-        ...loserGame,
-        number: 4,
-        second_player: winner === game.first_player ? game.second_player : game.first_player
-      }
-    } else if (game.number === 3) {
-      winnerGame = {
-        ...winnerGame,
-        number: 2,
-        second_player: winner
-      }
-      loserGame = {
-        ...loserGame,
-        number: 4,
-        first_player: winner === game.first_player ? game.second_player : game.first_player
-      }
-    } else if (game.number === 4) {
-      winnerGame = {
-        ...winnerGame,
-        number: 1,
-        second_player: winner
-      }
-      loserGame = {
-        ...loserGame,
-        number: 3,
-        first_player: winner === game.first_player ? game.second_player : game.first_player
-      }
-    }
-    return forkJoin([this.gamesService.updateOne(winnerGame), this.gamesService.updateOne(loserGame)])
+    const winnerGame = [
+      {tower: 2, number: 1, first_player: winner},
+      {tower: 2, number: 2, first_player: winner},
+      {tower: 2, number: 2, second_player: winner},
+      {tower: 2, number: 1, second_player: winner}
+    ]
+    const loserGame = [
+      {tower: 2, number: 3, second_player: this.getLoser(game, winner)},
+      {tower: 2, number: 4, second_player: this.getLoser(game, winner)},
+      {tower: 2, number: 4, first_player: this.getLoser(game, winner)},
+      {tower: 2, number: 3, first_player: this.getLoser(game, winner)}
+    ]
+    return forkJoin([this.gamesService.updateOne(winnerGame[game.number-1]), this.gamesService.updateOne(loserGame[game.number-1])])
   }
+
+  getLoser(game: Game, winner: number | undefined) {
+    return winner === game.first_player ? game.second_player : game.first_player
+  }
+
   updateTowerTwo(game: Game, winner: number) {}
   updateTowerThree(game: Game, winner: number) {}
 
